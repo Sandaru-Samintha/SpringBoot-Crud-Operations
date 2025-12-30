a6c7dce7cb2fc1887075bfe0f1cbeea5a4865476
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-12-30 10:54:38 +0530

    implement the fetch data in database

--- src/main/java/com/example/FirstProject/controller/UserController.java
+++ src/main/java/com/example/FirstProject/controller/UserController.java
@@ -6,6 +6,8 @@ import com.example.FirstProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v1/user")
 public class UserController {
@@ -16,6 +18,11 @@ public class UserController {
     @GetMapping("/getUser")
     public String getUser(){
         return "Hi Sandaru Saminha";
+    }
+
+    @GetMapping("/getUsers")
+    public List<UserDto> getUsers(){
+        return userService.getAllUsers();
     }
     @PostMapping("/saveUser")
     public UserDto saveUser(@RequestBody UserDto userDto){
--- src/main/java/com/example/FirstProject/service/UserService.java
+++ src/main/java/com/example/FirstProject/service/UserService.java
@@ -4,10 +4,12 @@ import com.example.FirstProject.dto.UserDto;
 import com.example.FirstProject.entity.User;
 import com.example.FirstProject.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -21,5 +23,10 @@ public class UserService {
     public UserDto saveUser(UserDto userDto){
         userRepository.save(modelMapper.map(userDto, User.class));
         return  userDto;
+    }
+
+    public List<UserDto> getAllUsers(){
+        List<User> userList = userRepository.findAll();
+        return modelMapper.map(userList,new TypeToken<List<UserDto>>(){}.getType());
     }
 }
